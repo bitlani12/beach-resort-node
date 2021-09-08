bc255b74ebc0f282792c8c3654204a3b53b4cadf
bitlani12 <44723246+bitlani12@users.noreply.github.com>
2021-09-09 00:09:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require('cors')
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
